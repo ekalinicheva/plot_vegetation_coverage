3c46e5d05663a452905175c9579459f2b0f0c2a2
ekalinicheva <e.kalinicheva@bk.ru>
2021-05-07 14:28:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
-pip install torchnet
-pip install laspy
+from setuptools import setup, find_packages
 
-
-# Install torch_scatter
-import torch
-
-def format_pytorch_version(version):
-  return version.split('+')[0]
-
-TORCH_version = torch.__version__
-TORCH = format_pytorch_version(TORCH_version)
-
-def format_cuda_version(version):
-  return 'cu' + version.replace('.', '')
-
-CUDA_version = torch.version.cuda
-CUDA = format_cuda_version(CUDA_version)
-
-pip install torch-scatter     -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-pip install torch-sparse      -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-pip install torch-cluster     -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
-pip install torch-geometric
+setup(
+    name='plot_vegetation_coverage',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='ekaterina',
+    author_email='',
+    description=''
+)
